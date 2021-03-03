f25426e7348b20890d77e07741a6a8cc35abb309
alamin853 <oriam853@gmail.com>
2021-03-03 19:30:40 +0600

    Nested Scope

---
+++ nestedScope.js
@@ -0,0 +1,14 @@
+var x = 50
+
+function test(){
+    // var x = 40
+    // console.log(x)
+
+    function nested(){
+        var y = 60
+        console.log(x)
+    }
+    // console.log(y)
+    nested()
+}
+test()
